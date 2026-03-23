67e1d39e4641bcc4d492946f6b824fcb06416226
Ricolito Cantorias <ricomagnifico018@gmail.com>
2026-03-23 23:11:19 +0800

    add find user

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,8 +51,8 @@ class User extends Authenticatable implements JWTSubject
 
             $refreshTokens = $this->refreshTokens();
 
-            if ($refreshTokens->count() > $maxAllowableRefreshTokens) {
-                $refreshTokens->oldest()->first()->delete();
+            if ($refreshTokens->count() >= $maxAllowableRefreshTokens) {
+               $refreshTokens->oldest()->limit(1)?->delete(); 
             }
 
             RefreshToken::create([
